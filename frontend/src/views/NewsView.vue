0ac2b77250a4c6517be0a4be26df781285721655
Pedram <pedram.hejazi.kenari@iths.se>
2024-05-02 13:41:04 +0200

    additional changes

--- frontend/src/views/NewsView.vue
+++ frontend/src/views/NewsView.vue
@@ -1,5 +1,21 @@
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      var: [],
+    };
+  },
+  created() {},
 
-<template></template>
+  methods: {
+    function1() {},
+    function2() {},
+  },
+};
+</script>
+
+<template>
+  <h1>The latest news</h1>
+</template>
 
 <script></script>
