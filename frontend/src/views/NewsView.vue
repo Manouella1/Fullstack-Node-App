508f7a54b46f60d07b9937fa64691efcc79d5b99
Pedram <pedram.hejazi.kenari@iths.se>
2024-05-03 19:33:14 +0200

    newsModel, newsView.vue file added

--- frontend/src/views/NewsView.vue
+++ frontend/src/views/NewsView.vue
@@ -2,22 +2,53 @@
 export default {
   data() {
     return {
-      var: [],
+      articles: [],
     };
   },
   created() {
-    fetch("https://localhost:3000/news").then().then();
+    this.getArticles();
   },
 
   methods: {
-    function1() {},
-    function2() {},
+    getArticles() {
+      fetch("http://localhost:3000/news")
+        .then((resp) => resp.json())
+        .then((result) => {
+          let containNews = [];
+          for (let i = 0; i < result.length; i++) {
+            containNews.push(result[i]);
+          }
+          this.articles = containNews;
+          console.log(this.articles);
+        });
+    },
   },
 };
 </script>
 
 <template>
-  <h1>The latest news</h1>
+  <h1 style="text-align: center">The latest news and blogs from our users</h1>
+
+  <article
+    class="articleDisp"
+    v-for="(article, index) in articles"
+    :key="index"
+  >
+    <h2>{{ article.title }}</h2>
+    <h3>{{ article.author }}</h3>
+    <p>{{ article.text }}</p>
+    <p>Tags: {{ article.tags.join(" ,") }}</p>
+    <p>{{ article.date }}</p>
+  </article>
+
+  <section></section>
 </template>
 
-<style></style>
+<style>
+.articleDisp {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 50px;
+}
+</style>
